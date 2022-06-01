da4b9921c21c52426daaa2237a1348c6ac3a2199
nedsociety <nedsociety@users.noreply.github.com>
2022-06-01 12:44:31 +0900

    Save changed files only for #38 and #39

--- snippets/issue38_mass_edits_de.py
+++ snippets/issue38_mass_edits_de.py
@@ -35,6 +35,8 @@ for path in glob('locale/**/de.po'):
     dict_original, _, sourcelocation = readpo(Path(path).parent / 'en.po')
     dict_translated, _, _ = readpo(path)
 
+    changed = False
+
     for key in list(dict_translated):
         entry_translated = dict_translated[key]
         if entry_translated.obsolete:
@@ -45,5 +47,7 @@ for path in glob('locale/**/de.po'):
         if target is None:
             continue
         dict_translated[key] = entry_translated._replace(value=target)
-    
-    writepo(path, dict_translated.values(), sourcelocation)
+        changed = True
+
+    if changed:
+        writepo(path, dict_translated.values(), sourcelocation)
