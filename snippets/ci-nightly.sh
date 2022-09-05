a77236b692fee4ffaf5609c94e4820b188b52e92
nedsociety <nedsociety@users.noreply.github.com>
2022-09-06 06:27:24 +0900

    ci: remove an unnecessary step, fix directory missing error

--- snippets/ci-nightly.sh
+++ snippets/ci-nightly.sh
@@ -4,9 +4,9 @@ set -x
 set -e
 
 mvloc batch-apply $1
-mv report.txt "packages/batch-apply-report-$1.txt"
-
 mvloc package $1
+
+mv report.txt "packages/batch-apply-report-$1.txt"
 
 pushd "output-$1"
 zip -r "../packages/xml_only-$1.zip" *
