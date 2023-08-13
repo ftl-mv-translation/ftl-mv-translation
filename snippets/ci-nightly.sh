5bdb9769fbee390f3172078d7adfb6259574ed75
nedsociety <nedsociety@gmail.com>
2023-08-13 15:34:41 +0900

    adding metadata to releases

--- snippets/ci-nightly.sh
+++ snippets/ci-nightly.sh
@@ -7,6 +7,7 @@ mvloc batch-apply $1
 mvloc package $1
 
 mv report.txt "packages/batch-apply-report-$1.txt"
+cp "output-$1/mod-appendix/metadata.xml" "packages/metadata-$1.xml"
 
 pushd "output-$1"
 zip -r "../packages/xml_only-$1.zip" *
