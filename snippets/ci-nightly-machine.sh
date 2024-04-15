4821f2625e52dcc02835e0363bea80fabffed1af
ranhai613 <85445781+ranhai613@users.noreply.github.com>
2024-04-16 00:06:17 +0900

    Fix matadata.xml name for MT

--- snippets/ci-nightly-machine.sh
+++ snippets/ci-nightly-machine.sh
@@ -7,7 +7,7 @@ mvloc batch-apply $1 -m
 mvloc package $1 -m
 
 mv report.txt "packages/batch-apply-report-$1-machine.txt"
-cp "output-$1/mod-appendix/metadata.xml" "packages/metadata-$1-machine.xml"
+cp "output-$1/mod-appendix/metadata.xml" "packages/metadata-$1.xml"
 
 pushd "output-$1"
 zip -r "../packages/xml_only-$1-machine.zip" *
