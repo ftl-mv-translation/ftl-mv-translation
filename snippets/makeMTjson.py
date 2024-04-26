1951d8bc2d40e4c79eb6b99455acc5aed8a005de
ranhai613 <hulanhai613@gmail.com>
2024-04-26 15:19:45 +0900

    Update MT

--- snippets/makeMTjson.py
+++ snippets/makeMTjson.py
@@ -2,6 +2,7 @@ from mvlocscript.fstools import glob_posix
 from mvlocscript.potools import readpo
 from sys import argv
 from json import dump
+from pathlib import Path
 
 def main():
     if len(argv) != 3:
@@ -16,15 +17,26 @@ def main():
     dict_temp = {}
     for filepath_original in glob_posix(globpattern_original):
             dict_original, _, _ = readpo(filepath_original)
-            dict_original = {entry.value: '' for entry in dict_original.values()}
-            dict_temp.update(dict_original)
+            dict_map = {}
+            try:
+                dict_hand, _, _ = readpo(f'locale/{Path(filepath_original).parent.parent.name}/{Path(filepath_original).parent.name}/{argv[1]}.po')
+                for key, entry in dict_original.items():
+                     dict_map[entry.value] = dict_hand.get(key, '')
+                for key in dict_map:
+                     if dict_map[key] == '':
+                          continue
+                     dict_map[key] = dict_map[key].value
+            except Exception as e:
+                 print(e)
+                 dict_map = {entry.value: '' for entry in dict_original.values()}
+            dict_temp.update(dict_map)
     data_dict['translation'] = [
         {
             'original': en,
-            'deepl': '',
+            'deepl': hand,
             'machine': ''
         }
-        for en in dict_temp.keys()
+        for en, hand in dict_temp.items()
     ]
             
     with open(f'machine-json/machine-{argv[1]}-{argv[2]}.json', 'wt') as f:
