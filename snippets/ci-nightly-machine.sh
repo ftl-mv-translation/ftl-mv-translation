783fed26d99abac8e559a0fd93164916776cd2b6
ranhai613 <hulanhai613@gmail.com>
2024-04-30 14:21:59 +0900

    add matadata.xml for mvinstaller

--- snippets/ci-nightly-machine.sh
+++ snippets/ci-nightly-machine.sh
@@ -7,7 +7,11 @@ mvloc batch-apply $1 -m
 mvloc package $1 -m
 
 mv report.txt "packages/batch-apply-report-$1-machine.txt"
-cp "output-$1/mod-appendix/metadata.xml" "packages/metadata-$1.xml"
+if [-e "metadata.xml/$1.xml"]; then
+    cp "metadata.xml/$1.xml" "packages/metadata-$1.xml"
+else
+    cp "output-$1/mod-appendix/metadata.xml" "packages/metadata-$1.xml"
+fi
 
 pushd "output-$1"
 zip -r "../packages/xml_only-$1-machine.zip" *
