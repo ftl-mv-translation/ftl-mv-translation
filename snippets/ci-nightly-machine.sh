c3ac16098136683463a184e46c3af11ddb29dc33
ranhai613 <85445781+ranhai613@users.noreply.github.com>
2024-05-01 00:58:06 +0900

    fix nightly.sh

--- snippets/ci-nightly-machine.sh
+++ snippets/ci-nightly-machine.sh
@@ -7,7 +7,7 @@ mvloc batch-apply $1 -m
 mvloc package $1 -m
 
 mv report.txt "packages/batch-apply-report-$1-machine.txt"
-if [-e "metadata.xml/$1.xml"]; then
+if [-f /metadata.xml/$1.xml]; then
     cp "metadata.xml/$1.xml" "packages/metadata-$1.xml"
 else
     cp "output-$1/mod-appendix/metadata.xml" "packages/metadata-$1.xml"
